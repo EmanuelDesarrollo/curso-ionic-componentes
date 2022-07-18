06480efd0d454fec0db9b0dbe442fe49ec01795d
Emanuel Buendia <emanuel_09_27@hotmail.com>
2022-07-18 08:27:16 -0500

    Seccion 5 - Popover

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { HeaderComponent } from './header/header.component';
+import { PopoverInfoComponent } from './popover-info/popover-info.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent],
-  exports: [HeaderComponent],
+  declarations: [
+    HeaderComponent,
+    PopoverInfoComponent
+  ],
+  exports: [
+    HeaderComponent,
+    PopoverInfoComponent
+  ],
   imports: [
     CommonModule, IonicModule
   ]
